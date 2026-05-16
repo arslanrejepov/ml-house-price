65df626517d1ea4c7a36d0c8cd68b1bf7e0e448d
Arslan Rejebov <arslanrejepov174@gmail.com>
2026-05-17 01:28:19 +0800

    Update

--- frontend/src/services/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export async function predictRent(params) {
-  const { data } = await axios.post('/api/predict', params);
-  return data; // expects { price, low, high }
-}
